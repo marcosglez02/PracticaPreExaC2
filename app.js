cdb4370a4bc6610991b76a1d5a1ea725cbf8e168
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2023-02-27 18:45:56 -0700

    Realizar archivo ejs para obtener los datos

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
+const misRutas = require("./router/index");
+const path = require("path");
 
 app.use(express.static(__dirname+'/public'))
-
-app.get('/',(req,res)=>{
-    res.render('index.ejs')
- });
-
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(misRutas)
 
 // Archivo de error en caso de no encontrar la página
 app.use((req,res,next)=>{res.status(404).sendFile(__dirname+'/public/error.html')})
--- router/index.js
+++ router/index.js
@@ -3,7 +3,22 @@ const router = express.Router();
 const bodyParse = require("body-parser");
 
 router.get('/',(req,res)=>{
-    res.render('index.ejs')
+    const valores = {
+        precio: req.query.precio,
+        años: req.query.años,
+        tipoViaje: req.query.tipoViaje
+     }
+    res.render('index.ejs',valores)
  });
+ router.post('/',(req,res)=>{
+    const valores = {
+        precio: req.body.precio,
+        años: req.body.años,
+        tipoViaje: req.body.tipoViaje
+     }
+    res.render('index.ejs',valores)
+ });
+
+
 
 module.exports=router;
