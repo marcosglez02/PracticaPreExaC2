6960cb7c2624982d4af8a4cc1f656c1c3ac0f701
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2023-02-27 17:50:09 -0700

    Generación carpeta views y formulario principal

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.get('/',(req,res)=>{
     res.render('index.ejs')
  });
 
+
+// Archivo de error en caso de no encontrar la página
+app.use((req,res,next)=>{res.status(404).sendFile(__dirname+'/public/error.html')})
 // Escuchar al servidor por el puerto 500
 const puerto = 500;
 app.listen(puerto,()=>{console.log("Iniciando puerto 500")});
